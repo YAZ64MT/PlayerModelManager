c0e18b8aa5d95ad685636f0af98de034721b2450
Neirn <neirny@gmail.com>
2025-05-20 21:14:29 -0700

    model_common.h now has some funcs

--- include/model_common.h
+++ include/model_common.h
@@ -1,6 +1,9 @@
 #ifndef __MODEL_COMMON_H__
 #define __MODEL_COMMON_H__
 
+#include "playermodelmanager.h"
 
+void initFormProxy(Link_FormProxy *formProxy);
+void refreshFormProxy(Link_FormProxy *formProxy);
 
 #endif
