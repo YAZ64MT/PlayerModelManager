45f461dec22d259a7d5bcc12f00306c9eb74ad14
Neirn <neirny@gmail.com>
2025-05-24 19:44:23 -0700

    initial ooto impl

--- src/ml64compat_mm.c
+++ src/ml64compat_mm.c
@@ -5,7 +5,10 @@
 #include "playermodelmanager_mm.h"
 #include "playermodelmanager_utils.h"
 #include "defines_mmo.h"
+#include "defines_ooto.h"
 #include "zobjutils.h"
+#include "model_common.h"
+#include 
 
 void setupFaceTextures(Link_ModelInfo *modelInfo, u8 *zobj) {
     for (u32 i = 0; i < PLAYER_EYES_MAX; ++i) {
@@ -106,4 +109,49 @@ void setupZobjMmo(Link_ModelInfo *modelInfo, u8 *zobj) {
     SET_MMO_MODEL(FPS_LHAND, LFIST);
     SET_MMO_MODEL(FPS_RFOREARM, DF_COMMAND);
     QSET_MMO_MODEL(FPS_RHAND);
+}
+
+#define SET_OOTO_CHILD_MODEL(dest, src) SET_Z64O_MODEL(dest, src, OOTO_CHILD)
+#define QSET_OOTO_CHILD_MODEL(dlName) SET_OOTO_CHILD_MODEL(dlName, dlName)
+
+void setupZobjOoTOChild(Link_ModelInfo *modelInfo, u8 *zobj) {
+
+    clearLinkModelInfo(modelInfo);
+
+    repointZobj(zobj, MMO_LUT_DL_WAIST, MMO_LUT_DL_DF_COMMAND);
+
+    handleZobjSkeleton(modelInfo, zobj);
+
+    modelInfo->equipMtx[LINK_EQUIP_MATRIX_SWORD_KOKIRI_BACK] = (Mtx *)&zobj[OOTO_CHILD_MATRIX_SWORD_BACK];
+    modelInfo->equipMtx[LINK_EQUIP_MATRIX_SHIELD_HERO_BACK] = (Mtx *)&zobj[OOTO_CHILD_MATRIX_SHIELD_BACK];
+
+    QSET_OOTO_CHILD_MODEL(WAIST);
+    QSET_OOTO_CHILD_MODEL(RTHIGH);
+    QSET_OOTO_CHILD_MODEL(RSHIN);
+    QSET_OOTO_CHILD_MODEL(RFOOT);
+    QSET_OOTO_CHILD_MODEL(LTHIGH);
+    QSET_OOTO_CHILD_MODEL(LSHIN);
+    QSET_OOTO_CHILD_MODEL(LFOOT);
+    QSET_OOTO_CHILD_MODEL(HEAD);
+    QSET_OOTO_CHILD_MODEL(HAT);
+    QSET_OOTO_CHILD_MODEL(COLLAR);
+    QSET_OOTO_CHILD_MODEL(LSHOULDER);
+    QSET_OOTO_CHILD_MODEL(LFOREARM);
+    QSET_OOTO_CHILD_MODEL(RSHOULDER);
+    QSET_OOTO_CHILD_MODEL(RFOREARM);
+    QSET_OOTO_CHILD_MODEL(TORSO);
+    QSET_OOTO_CHILD_MODEL(LHAND);
+    QSET_OOTO_CHILD_MODEL(LFIST);
+    QSET_OOTO_CHILD_MODEL(LHAND_BOTTLE);
+    QSET_OOTO_CHILD_MODEL(RHAND);
+    QSET_OOTO_CHILD_MODEL(RFIST);
+    QSET_OOTO_CHILD_MODEL(SWORD_KOKIRI_SHEATH);
+    QSET_OOTO_CHILD_MODEL(SWORD_KOKIRI_HILT);
+    QSET_OOTO_CHILD_MODEL(SWORD_KOKIRI_BLADE);
+    SET_OOTO_CHILD_MODEL(SHIELD_HERO, SHIELD_DEKU);
+    QSET_OOTO_CHILD_MODEL(OCARINA_TIME);
+    SET_OOTO_CHILD_MODEL(FPS_LFOREARM, LFOREARM);
+    SET_OOTO_CHILD_MODEL(FPS_LHAND, LFIST);
+    modelInfo->models[LINK_DL_FPS_RFOREARM] = dfCommand;
+    QSET_OOTO_CHILD_MODEL(FPS_RHAND);
 }
