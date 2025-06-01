10867076997e452b6482bb8aa2cf1f8a297dd4bb
Neirn <neirny@gmail.com>
2025-06-01 14:01:10 -0700

    Update zproxy_deku_stick.c
    Replace ZProxyManager with ZModelReplacer

--- src/zproxy_deku_stick.c
+++ src/zproxy_deku_stick.c
@@ -1,9 +1,9 @@
 #include "global.h"
 #include "recomputils.h"
-#include "zproxy_manager.h"
+#include "modelreplacer_api.h"
 #include "assets/objects/gameplay_keep/gameplay_keep.h"
 
-RECOMP_CALLBACK(".", ZProxyManager_onInit)
+RECOMP_CALLBACK(YAZMT_Z64_MODEL_REPLACER_MOD_NAME, ZModelReplacer_onReady)
 void reserveDekuStick() {
-    ZProxyManager_reserveVanillaDisplayList(GAMEPLAY_KEEP, &gDekuStickDL);
+    ZModelReplacer_preregisterDL(GAMEPLAY_KEEP, &gDekuStickDL);
 }
