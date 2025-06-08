9b053fd94b0f0ef22026ab4a69ec5ea315beb6ed
Neirn <neirny@gmail.com>
2025-06-08 09:29:00 -0700

    Combine external dl replacements into one file

--- src/externaldisplaylists.c
+++ src/externaldisplaylists.c
@@ -2,15 +2,17 @@
 #include "externaldisplaylists.h"
 #include "modelreplacer_api.h"
 #include "assets/objects/gameplay_keep/gameplay_keep.h"
-#include "model_common.h"
+#include "assets/objects/object_link_child/object_link_child.h"
 
 ZModelReplacerHandle gProxyBottleGlass;
 ZModelReplacerHandle gProxyBottleContents;
 ZModelReplacerHandle gProxyDekuStick;
+ZModelReplacerHandle gProxyBowString;
 
 ZMODELREPLACER_CALLBACK_REGISTER_REPLACERS
-void setupGameplayKeepDLReplacements() {
+void setupExternalReplacements() {
     gProxyBottleGlass = ZModelReplacer_registerReplacer(GAMEPLAY_KEEP, gBottleGlassDL, NULL);
     gProxyBottleContents = ZModelReplacer_registerReplacer(GAMEPLAY_KEEP, gBottleContentsDL, NULL);
     gProxyDekuStick = ZModelReplacer_registerReplacer(GAMEPLAY_KEEP, gDekuStickDL, NULL);
+    gProxyBowString = ZModelReplacer_registerReplacer(OBJECT_LINK_CHILD, object_link_child_DL_017818, NULL);
 }
