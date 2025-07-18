YAZ64MT/PlayerModelManager

commit 002a8b8e453470abb56e6769637631038f9b3b64

only run age property replacing code if player is human

src/mm_adultfixes.c
@@ -80,36 +80,38 @@ extern LinkAnimationHeader gPlayerAnim_clink_demo_doorA_link;
 extern LinkAnimationHeader gPlayerAnim_clink_demo_doorB_link;
 
 void handleAgeProps(PlayState *play) {
-    if (IS_HUMAN_ADULT_LINK_MODEL) {
-        sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gAdultLinkAgeProps;
+    if (GET_PLAYER_FORM == PLAYER_FORM_HUMAN) {
+        if (IS_HUMAN_ADULT_LINK_MODEL) {
+            sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gAdultLinkAgeProps;
 
-        for (int i = 0; i < PLAYER_ANIMTYPE_MAX; i++) {
-            D_8085BE84[PLAYER_ANIMGROUP_doorA][i] = D_8085BE84[PLAYER_ANIMGROUP_doorA_free][i];
-            D_8085BE84[PLAYER_ANIMGROUP_doorB][i] = D_8085BE84[PLAYER_ANIMGROUP_doorB_free][i];
+            for (int i = 0; i < PLAYER_ANIMTYPE_MAX; i++) {
+                D_8085BE84[PLAYER_ANIMGROUP_doorA][i] = D_8085BE84[PLAYER_ANIMGROUP_doorA_free][i];
+                D_8085BE84[PLAYER_ANIMGROUP_doorB][i] = D_8085BE84[PLAYER_ANIMGROUP_doorB_free][i];
+            }
+
+            sPlayerMass[PLAYER_FORM_HUMAN] = sPlayerMass[PLAYER_FORM_ZORA];
+
+            for (int i = 0; i < NUM_BOOT_PROPERTIES; ++i) {
+                PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = PLAYER_BOOTS_ARR[PLAYER_BOOTS_ZORA_LAND][i];
+            }
+
+            GET_PLAYER(play)->ageProperties = &gAdultLinkAgeProps;
+        } else {
+            sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gVanillaHumanLinkAgeProps;
+
+            for (int i = 0; i < PLAYER_ANIMTYPE_MAX; i++) {
+                D_8085BE84[PLAYER_ANIMGROUP_doorA][i] = &gPlayerAnim_clink_demo_doorA_link;
+                D_8085BE84[PLAYER_ANIMGROUP_doorB][i] = &gPlayerAnim_clink_demo_doorB_link;
+            }
+
+            sPlayerMass[PLAYER_FORM_HUMAN] = gVanillaPlayerMass;
+
+            for (int i = 0; i < NUM_BOOT_PROPERTIES; ++i) {
+                PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = gVanillaHumanBootProperties[i];
+            }
+
+            GET_PLAYER(play)->ageProperties = &gVanillaHumanLinkAgeProps;
         }
-
-        sPlayerMass[PLAYER_FORM_HUMAN] = sPlayerMass[PLAYER_FORM_ZORA];
-
-        for (int i = 0; i < NUM_BOOT_PROPERTIES; ++i) {
-            PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = PLAYER_BOOTS_ARR[PLAYER_BOOTS_ZORA_LAND][i];
-        }
-
-        GET_PLAYER(play)->ageProperties = &gAdultLinkAgeProps;
-    } else {
-        sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gVanillaHumanLinkAgeProps;
-
-        for (int i = 0; i < PLAYER_ANIMTYPE_MAX; i++) {
-            D_8085BE84[PLAYER_ANIMGROUP_doorA][i] = &gPlayerAnim_clink_demo_doorA_link;
-            D_8085BE84[PLAYER_ANIMGROUP_doorB][i] = &gPlayerAnim_clink_demo_doorB_link;
-        }
-
-        sPlayerMass[PLAYER_FORM_HUMAN] = gVanillaPlayerMass;
-
-        for (int i = 0; i < NUM_BOOT_PROPERTIES; ++i) {
-            PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = gVanillaHumanBootProperties[i];
-        }
-
-        GET_PLAYER(play)->ageProperties = &gVanillaHumanLinkAgeProps;
     }
 }
 

commit 0c5888fef550383a7403240aa03e986922cbb7a8

Player no longer floats when taking off deku mask
Fixes regression introduced in 002a8b8 and 65f3568

src/mm_adultfixes.c
@@ -80,7 +80,9 @@ extern LinkAnimationHeader gPlayerAnim_clink_demo_doorA_link;
 extern LinkAnimationHeader gPlayerAnim_clink_demo_doorB_link;
 
 void handleAgeProps(PlayState *play) {
-    if (GET_PLAYER_FORM == PLAYER_FORM_HUMAN) {
+    Player *player = GET_PLAYER(play);
+
+    if (player->transformation == PLAYER_FORM_HUMAN && player->skelAnime.animation != &gPlayerAnim_pg_maskoffstart) {
         if (IS_HUMAN_ADULT_LINK_MODEL) {
             sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gAdultLinkAgeProps;
 
@@ -95,7 +97,7 @@ void handleAgeProps(PlayState *play) {
                 PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = PLAYER_BOOTS_ARR[PLAYER_BOOTS_ZORA_LAND][i];
             }
 
-            GET_PLAYER(play)->ageProperties = &gAdultLinkAgeProps;
+            player->ageProperties = &gAdultLinkAgeProps;
         } else {
             sPlayerAgeProperties[PLAYER_FORM_HUMAN] = gVanillaHumanLinkAgeProps;
 
@@ -110,7 +112,7 @@ void handleAgeProps(PlayState *play) {
                 PLAYER_BOOTS_ARR[PLAYER_BOOTS_HYLIAN][i] = gVanillaHumanBootProperties[i];
             }
 
-            GET_PLAYER(play)->ageProperties = &gVanillaHumanLinkAgeProps;
+            player->ageProperties = &gVanillaHumanLinkAgeProps;
         }
     }
 }