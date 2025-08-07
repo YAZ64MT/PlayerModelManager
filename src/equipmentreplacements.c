db69046836b09a49ac3439e481f7334914ae4f96
Neirn <neirny@gmail.com>
2025-08-07 12:15:03 -0700

    Initial override setup

--- src/equipmentreplacements.c
+++ src/equipmentreplacements.c
@@ -113,23 +113,104 @@ void createBottleOverride(Link_EquipmentReplacement *equipRepl) {
     equipRepl->mtxOverrides = NULL;
 }
 
+void createPipesOverride(Link_EquipmentReplacement *equipRepl) {
+    equipRepl->numDLOverrides = 6;
+    equipRepl->DLOverrides = allocDLOverrides(equipRepl->numDLOverrides);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[0], LINK_DL_PIPE_A);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[1], LINK_DL_PIPE_LEFT);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[2], LINK_DL_PIPE_RIGHT);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[3], LINK_DL_PIPE_DOWN);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[4], LINK_DL_PIPE_UP);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[5], LINK_DL_PIPE_MOUTH);
+
+    equipRepl->numMtxOverrides = 0;
+    equipRepl->mtxOverrides = NULL;
+}
+
+void createDrumsOverride(Link_EquipmentReplacement *equipRepl) {
+    equipRepl->numDLOverrides = 6;
+    equipRepl->DLOverrides = allocDLOverrides(equipRepl->numDLOverrides);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[0], LINK_DL_DRUM_A);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[1], LINK_DL_DRUM_LEFT);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[2], LINK_DL_DRUM_RIGHT);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[3], LINK_DL_DRUM_DOWN);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[4], LINK_DL_DRUM_UP);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[5], LINK_DL_DRUM_STRAP);
+
+    equipRepl->numMtxOverrides = 0;
+    equipRepl->mtxOverrides = NULL;
+}
+
+void createSingleDLOverride(Link_EquipmentReplacement *equipRepl, Link_DisplayList id) {
+    equipRepl->numDLOverrides = 1;
+    equipRepl->DLOverrides = allocDLOverrides(equipRepl->numDLOverrides);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[0], id);
+
+    equipRepl->numMtxOverrides = 0;
+    equipRepl->mtxOverrides = NULL;
+}
+
+void createDoubleDLOverride(Link_EquipmentReplacement *equipRepl, Link_DisplayList id1, Link_DisplayList id2) {
+    equipRepl->numDLOverrides = 2;
+    equipRepl->DLOverrides = allocDLOverrides(equipRepl->numDLOverrides);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[0], id1);
+    Link_DisplayListOverride_init(&equipRepl->DLOverrides[1], id2);
+
+    equipRepl->numMtxOverrides = 0;
+    equipRepl->mtxOverrides = NULL;
+}
+
 void Link_EquipmentReplacements_init(Link_EquipmentReplacements *equipRepls) {
-#define CREATE_SWORD_OVERRIDE(swordNum) createSwordOverride(&equipRepls[LINK_DL_REPLACE_SWORD##swordNum], LINK_DL_SWORD##swordNum##_HILT, LINK_DL_SWORD##swordNum##_BLADE, LINK_DL_SWORD##swordNum##_SHEATH, LINK_EQUIP_MATRIX_SWORD##swordNum##_BACK);
+#define CREATE_SWORD_OVERRIDE(swordNum) createSwordOverride(&equipRepls->replacements[LINK_DL_REPLACE_SWORD##swordNum], LINK_DL_SWORD##swordNum##_HILT, LINK_DL_SWORD##swordNum##_BLADE, LINK_DL_SWORD##swordNum##_SHEATH, LINK_EQUIP_MATRIX_SWORD##swordNum##_BACK);
     CREATE_SWORD_OVERRIDE(1);
     CREATE_SWORD_OVERRIDE(2);
     CREATE_SWORD_OVERRIDE(3);
-    createBgsOverride(&equipRepls[LINK_DL_REPLACE_SWORD4]);
+    createBgsOverride(&equipRepls->replacements[LINK_DL_REPLACE_SWORD4]);
     CREATE_SWORD_OVERRIDE(5);
 #undef CREATE_SWORD_OVERRIDE
 
-#define CREATE_SHIELD_OVERRIDE(num) createShieldOverride(&equipRepls[LINK_DL_REPLACE_SHIELD##num], LINK_DL_SHIELD##num, LINK_EQUIP_MATRIX_SHIELD##num##_BACK);
+#define CREATE_SHIELD_OVERRIDE(num) createShieldOverride(&equipRepls->replacements[LINK_DL_REPLACE_SHIELD##num], LINK_DL_SHIELD##num, LINK_EQUIP_MATRIX_SHIELD##num##_BACK);
     CREATE_SHIELD_OVERRIDE(1);
     CREATE_SHIELD_OVERRIDE(2);
     CREATE_SHIELD_OVERRIDE(3);
 #undef CREATE_SHIELD_OVERRIDE
 
-    createBowOverride(&equipRepls[LINK_DL_REPLACE_BOW]);
-    createHookshotOverride(&equipRepls[LINK_DL_REPLACE_HOOKSHOT]);
-    createSlingshotOverride(&equipRepls[LINK_DL_REPLACE_SLINGSHOT]);
-    createBottleOverride(&equipRepls[LINK_DL_REPLACE_BOTTLE]);
+    createBowOverride(&equipRepls->replacements[LINK_DL_REPLACE_BOW]);
+    createHookshotOverride(&equipRepls->replacements[LINK_DL_REPLACE_HOOKSHOT]);
+    createSlingshotOverride(&equipRepls->replacements[LINK_DL_REPLACE_SLINGSHOT]);
+    createBottleOverride(&equipRepls->replacements[LINK_DL_REPLACE_BOTTLE]);
+    createPipesOverride(&equipRepls->replacements[LINK_DL_REPLACE_PIPES]);
+    createDrumsOverride(&equipRepls->replacements[LINK_DL_REPLACE_DRUMS]);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_DEKU_STICK], LINK_DL_DEKU_STICK);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_OCARINA_FAIRY], LINK_DL_OCARINA_FAIRY);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_OCARINA_TIME], LINK_DL_OCARINA_TIME);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_HAMMER], LINK_DL_HAMMER);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_GUITAR], LINK_DL_GUITAR);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_SKULL], LINK_DL_MASK_SKULL);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_SPOOKY], LINK_DL_MASK_SPOOKY);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GERUDO], LINK_DL_MASK_GERUDO);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_TRUTH], LINK_DL_MASK_TRUTH);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_KAFEIS_MASK], LINK_DL_MASK_KAFEIS_MASK);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_ALL_NIGHT], LINK_DL_MASK_ALL_NIGHT);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_BUNNY], LINK_DL_MASK_BUNNY);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_KEATON], LINK_DL_MASK_KEATON);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GARO], LINK_DL_MASK_GARO);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_ROMANI], LINK_DL_MASK_ROMANI);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_CIRCUS_LEADER], LINK_DL_MASK_CIRCUS_LEADER);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_COUPLE], LINK_DL_MASK_COUPLE);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_POSTMAN], LINK_DL_MASK_POSTMAN);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GREAT_FAIRY], LINK_DL_MASK_GREAT_FAIRY);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GIBDO], LINK_DL_MASK_GIBDO);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_DON_GERO], LINK_DL_MASK_DON_GERO);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_KAMARO], LINK_DL_MASK_KAMARO);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_CAPTAIN], LINK_DL_MASK_CAPTAIN);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_STONE], LINK_DL_MASK_STONE);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_BREMEN], LINK_DL_MASK_BREMEN);
+    createDoubleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_BLAST], LINK_DL_MASK_BLAST, LINK_DL_MASK_BLAST_COOLING_DOWN);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_SCENTS], LINK_DL_MASK_SCENTS);
+    createSingleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GIANT], LINK_DL_MASK_GIANT);
+    createDoubleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_DEKU], LINK_DL_MASK_DEKU, LINK_DL_MASK_DEKU_SCREAM);
+    createDoubleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_GORON], LINK_DL_MASK_GORON, LINK_DL_MASK_GORON_SCREAM);
+    createDoubleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_ZORA], LINK_DL_MASK_ZORA, LINK_DL_MASK_ZORA_SCREAM);
+    createDoubleDLOverride(&equipRepls->replacements[LINK_DL_REPLACE_MASK_FIERCE_DEITY], LINK_DL_MASK_FIERCE_DEITY, LINK_DL_MASK_FIERCE_DEITY_SCREAM);
 }
