57d736b69ca095f6882c1b1b8b47c7cc0030f829
Neirn <neirny@gmail.com>
2025-08-07 21:57:19 -0700

    Start work on equipment overrides

---
+++ include/equipmentoverrides.h
@@ -0,0 +1,24 @@
+#ifndef __EQUIPMENTOVERRIDES_H__
+#define __EQUIPMENTOVERRIDES_H__
+
+#include "PR/ultratypes.h"
+#include "modelmatrixids.h"
+
+typedef struct {
+    Link_DisplayList *overrides;
+    size_t count;
+} EquipmentOverrideDLs;
+
+typedef struct {
+    Link_EquipmentMatrix *overrides;
+    size_t count;
+} EquipmentOverrideMtxs;
+
+typedef struct {
+    EquipmentOverrideDLs dl;
+    EquipmentOverrideMtxs mtx;
+} EquipmentOverride;
+
+extern EquipmentOverride gEquipmentOverrideTable[LINK_DL_REPLACE_MAX];
+
+#endif
---
+++ src/equipmentoverrides.c
@@ -0,0 +1,26 @@
+#include "global.h"
+#include "equipmentoverrides.h"
+#include "modelmatrixids.h"
+
+static Link_DisplayList sSword1DLs[] = {LINK_DL_SWORD1_HILT, LINK_DL_SWORD1_BLADE, LINK_DL_SWORD1_SHEATH};
+static Link_DisplayList sSword2DLs[] = {LINK_DL_SWORD2_HILT, LINK_DL_SWORD2_BLADE, LINK_DL_SWORD2_SHEATH};
+static Link_DisplayList sSword3DLs[] = {LINK_DL_SWORD3_HILT, LINK_DL_SWORD3_BLADE, LINK_DL_SWORD3_SHEATH};
+static Link_DisplayList sSword4DLs[] = {LINK_DL_SWORD4_HILT, LINK_DL_SWORD4_BLADE, LINK_DL_SWORD4_SHEATH, LINK_DL_SWORD4_BLADE_BROKEN, LINK_DL_SWORD4_BLADE_FRAGMENT};
+static Link_DisplayList sSword5DLs[] = {LINK_DL_SWORD5_HILT, LINK_DL_SWORD5_BLADE, LINK_DL_SWORD5_SHEATH};
+static Link_EquipmentMatrix sSword1Mtxs[] = {LINK_EQUIP_MATRIX_SWORD1_BACK};
+static Link_EquipmentMatrix sSword2Mtxs[] = {LINK_EQUIP_MATRIX_SWORD2_BACK};
+static Link_EquipmentMatrix sSword3Mtxs[] = {LINK_EQUIP_MATRIX_SWORD3_BACK, LINK_EQUIP_MATRIX_SWORD3_PEDESTAL, LINK_EQUIP_MATRIX_SWORD3_PEDESTAL_GRABBED};
+static Link_EquipmentMatrix sSword4Mtxs[] = {LINK_EQUIP_MATRIX_SWORD4_BACK};
+static Link_EquipmentMatrix sSword5Mtxs[] = {LINK_EQUIP_MATRIX_SWORD5_BACK};
+
+static Link_DisplayList sShield1DLs[] = {LINK_DL_SHIELD1};
+static Link_DisplayList sShield2DLs[] = {LINK_DL_SHIELD2};
+static Link_DisplayList sShield3DLs[] = {LINK_DL_SHIELD3};
+static Link_EquipmentMatrix sShield1Mtxs = {LINK_EQUIP_MATRIX_SHIELD1_BACK, LINK_EQUIP_MATRIX_SHIELD1_ITEM};
+static Link_EquipmentMatrix sShield2Mtxs = {LINK_EQUIP_MATRIX_SHIELD2_BACK};
+static Link_EquipmentMatrix sShield3Mtxs = {LINK_EQUIP_MATRIX_SHIELD3_BACK};
+
+static Link_DisplayList sHookshotDLs[] = {LINK_DL_HOOKSHOT, LINK_DL_FPS_HOOKSHOT, LINK_DL_HOOKSHOT_CHAIN, LINK_DL_HOOKSHOT_HOOK, LINK_DL_HOOKSHOT_RETICLE};
+static Link_EquipmentMatrix sHookshotMtxs[] = {LINK_EQUIP_MATRIX_HOOKSHOT_CHAIN_AND_HOOK};
+
+static Link_DisplayList sBowDLs[] = {LINK_DL_BOW, LINK_DL_FPS_BOW, LINK_DL_BOW_STRING, LINK_DL_BOW_ARROW};
