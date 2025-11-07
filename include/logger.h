b3e55ad4e91938c06a32cd6224c33023d8e3749a
Neirn <neirny@gmail.com>
2025-11-06 18:42:27 -0800

    Change logging functions to macros
    Recomp doesn't have a vaprintf

--- include/logger.h
+++ include/logger.h
@@ -1,19 +1,45 @@
 #ifndef __LOGGING_H__
 #define __LOGGING_H__
 
+#include "recomputils.h"
+
 typedef enum {
-    LOGGER_LEVEL_ALL,
-    LOGGER_LEVEL_ERROR,
-    LOGGER_LEVEL_WARNING,
-    LOGGER_LEVEL_INFO,
     LOGGER_LEVEL_NONE,
+    LOGGER_LEVEL_INFO,
+    LOGGER_LEVEL_WARNING,
+    LOGGER_LEVEL_ERROR,
+    LOGGER_LEVEL_ALL,
 } LoggerLevel;
 
-void Logger_print(char *s, ...);
-void Logger_printLine(char *s, ...);
-void Logger_printInfo(char *s, ...);
-void Logger_printWarning(char *s, ...);
-void Logger_printError(char *s, ...);
+extern LoggerLevel gLoggerLevel;
+
+#define Logger_printLine(...)       \
+    {                               \
+        recomp_printf(__VA_ARGS__); \
+        recomp_printf("\n");        \
+    }
+
+#define Logger_printInfo(...)                        \
+    {                                                \
+        if (gLoggerLevel <= LOGGER_LEVEL_INFO) {     \
+            Logger_printLine("[INFO] " __VA_ARGS__); \
+        }                                            \
+    }
+
+#define Logger_printWarning(...)                     \
+    {                                                \
+        if (gLoggerLevel <= LOGGER_LEVEL_WARNING) {  \
+            recomp_printf("[WARNING] " __VA_ARGS__); \
+        }                                            \
+    }
+
+#define Logger_printError(...)                     \
+    {                                              \
+        if (gLoggerLevel <= LOGGER_LEVEL_ERROR) {  \
+            recomp_printf("[ERROR] " __VA_ARGS__); \
+        }                                          \
+    }
+
 void Logger_setLoggerLevel(LoggerLevel level);
 
 #endif
--- src/logger.c
+++ src/logger.c
@@ -2,60 +2,13 @@
 #include "recomputils.h"
 #include "libc/stdarg.h"
 
-static LoggerLevel sLogLevel = LOGGER_LEVEL_ALL;
-
-void Logger_print(char *s, ...) {
-    if (sLogLevel < LOGGER_LEVEL_NONE) {
-        va_list args;
-        va_start(args, s);
-
-        recomp_printf(s, args);
-    }
-}
-
-void Logger_printLine(char *s, ...) {
-    va_list args;
-    va_start(args, s);
-
-    Logger_print(s, args);
-    Logger_print("\n");
-}
-
-void Logger_printInfo(char *s, ...) {
-    if (sLogLevel <= LOGGER_LEVEL_INFO) {
-        va_list args;
-        va_start(args, s);
-
-        recomp_printf("[INFO] ");
-        Logger_printLine(s, args);
-    }
-}
-
-void Logger_printWarning(char *s, ...) {
-    if (sLogLevel <= LOGGER_LEVEL_WARNING) {
-        va_list args;
-        va_start(args, s);
-
-        recomp_printf("[WARNING] ");
-        Logger_printLine(s, args);
-    }
-}
-
-void Logger_printError(char *s, ...) {
-    if (sLogLevel <= LOGGER_LEVEL_ERROR) {
-        va_list args;
-        va_start(args, s);
-
-        recomp_printf("[ERROR] ");
-        Logger_printLine(s, args);
-    }
-}
+LoggerLevel gLoggerLevel = LOGGER_LEVEL_WARNING;
 
 void Logger_setLoggerLevel(LoggerLevel level) {
-    if (level < 0 || level >LOGGER_LEVEL_NONE) {
+    if (level < 0 || level > LOGGER_LEVEL_NONE) {
         Logger_printWarning("Called Logger_setLoggerLevel with invalid level %d", level);
         return;
     }
 
-    sLogLevel = level;
+    gLoggerLevel = level;
 }
