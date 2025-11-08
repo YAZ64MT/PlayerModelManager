10146eb456c86d263eb0768511fbdb7c0b9a27b2
Neirn <neirny@gmail.com>
2025-11-08 11:59:32 -0800

    Simplify ModelInfo struct
    Removed fields shared with ModelEntryForm and just included a pointer to a ModelEntryForm

--- include/modelinfo.h
+++ include/modelinfo.h
@@ -4,47 +4,30 @@
 #include "global.h"
 #include "modelmatrixids.h"
 #include "recompdata.h"
+#include "custommodelentry.h"
 
 typedef struct {
-    U32ValueHashmapHandle gfx;
-    U32ValueHashmapHandle mtx;
+    ModelEntryForm *modelEntryForm;
     U32ValueHashmapHandle gfxOverrides;
     U32ValueHashmapHandle mtxOverrides;
-    u64 flags;
-    FlexSkeletonHeader *skeleton;
-    FlexSkeletonHeader *shieldingSkeleton;
-    TexturePtr eyesTextures[PLAYER_EYES_MAX];
-    TexturePtr mouthTextures[PLAYER_MOUTH_MAX];
 } ModelInfo;
 
 void ModelInfo_init(ModelInfo *modelInfo);
+bool ModelInfo_hasModelEntry(ModelInfo *modelInfo);
 Gfx *ModelInfo_getGfx(ModelInfo *modelInfo, Link_DisplayList id);
 Mtx *ModelInfo_getMtx(ModelInfo *modelInfo, Link_EquipmentMatrix id);
-void ModelInfo_setEyesTextures(ModelInfo *modelInfo, TexturePtr eyeTextures[]);
+void ModelInfo_setModelEntryForm(ModelInfo *modelInfo, ModelEntryForm *modelEntry);
+ModelEntryForm *ModelInfo_getModelEntryForm(ModelInfo *modelInfo);
 TexturePtr ModelInfo_getEyesTexture(ModelInfo *modelInfo, PlayerEyeIndex i);
-void ModelInfo_setMouthTextures(ModelInfo *modelInfo, TexturePtr mouthTextures[]);
 TexturePtr ModelInfo_getMouthTexture(ModelInfo *modelInfo, PlayerMouthIndex i);
-void ModelInfo_setSkeleton(ModelInfo *modelInfo, FlexSkeletonHeader *skel);
-void ModelInfo_unsetSkeleton(ModelInfo *modelInfo);
-void ModelInfo_setShieldingSkeleton(ModelInfo *modelInfo, FlexSkeletonHeader *skel);
-void ModelInfo_unsetShieldingSkeleton(ModelInfo *modelInfo);
-void ModelInfo_setGfxMap(ModelInfo *modelInfo, U32ValueHashmapHandle gfxMap);
-void ModelInfo_unsetGfxMap(ModelInfo *modelInfo);
+FlexSkeletonHeader *ModelInfo_getSkeleton(ModelInfo *modelInfo);
+FlexSkeletonHeader *ModelInfo_getShieldingSkeleton(ModelInfo *modelInfo);
 bool ModelInfo_setGfxOverride(ModelInfo *modelInfo, Link_DisplayList id, Gfx *dl);
 bool ModelInfo_unsetGfxOverride(ModelInfo *modelInfo, Link_DisplayList id);
-void ModelInfo_setMtxMap(ModelInfo *modelInfo, U32ValueHashmapHandle mtxMap);
-void ModelInfo_unsetMtxMap(ModelInfo *modelInfo);
 bool ModelInfo_setMtxOverride(ModelInfo *modelInfo, Link_EquipmentMatrix id, Mtx *mtx);
 bool ModelInfo_unsetMtxOverride(ModelInfo *modelInfo, Link_EquipmentMatrix id);
-void ModelInfo_setFlags(ModelInfo *modelInfo, u64 flags);
-void ModelInfo_unsetFlags(ModelInfo *modelInfo, u64 flags);
-void ModelInfo_clearAllGfx(ModelInfo *modelInfo);
 void ModelInfo_clearAllGfxOverrides(ModelInfo *modelInfo);
-void ModelInfo_clearAllMtx(ModelInfo *modelInfo);
 void ModelInfo_clearAllMtxOverrides(ModelInfo *modelInfo);
-void ModelInfo_clearAllEyesTextures(ModelInfo *modelInfo);
-void ModelInfo_clearAllMouthTextures(ModelInfo *modelInfo);
-void ModelInfo_clearAllFlags(ModelInfo *modelInfo);
 bool ModelInfo_isAnyFlagEnabled(ModelInfo *modelInfo, u64 flags);
 
 #endif
--- src/modelinfo.c
+++ src/modelinfo.c
@@ -5,8 +5,7 @@
 #include "playermodelmanager_utils.h"
 
 void ModelInfo_init(ModelInfo *modelInfo) {
-    modelInfo->gfx = 0;
-    modelInfo->mtx = 0;
+    modelInfo->modelEntryForm = NULL;
     modelInfo->gfxOverrides = recomputil_create_u32_value_hashmap();
     modelInfo->mtxOverrides = recomputil_create_u32_value_hashmap();
     ModelInfo_clearFlags(modelInfo);
@@ -14,6 +13,10 @@ void ModelInfo_init(ModelInfo *modelInfo) {
     ModelInfo_unsetShieldingSkeleton(modelInfo);
     ModelInfo_clearEyesTextures(modelInfo);
     ModelInfo_clearMouthTextures(modelInfo);
+}
+
+bool ModelInfo_hasModelEntry(ModelInfo *modelInfo) {
+    return modelInfo->modelEntryForm;
 }
 
 Gfx *ModelInfo_getGfx(ModelInfo *modelInfo, Link_DisplayList id) {
@@ -24,8 +27,8 @@ Gfx *ModelInfo_getGfx(ModelInfo *modelInfo, Link_DisplayList id) {
     uintptr_t gfx = 0;
 
     if (!recomputil_u32_value_hashmap_get(modelInfo->gfxOverrides, id, &gfx)) {
-        if (modelInfo->gfx) {
-            recomputil_u32_value_hashmap_get(modelInfo->gfx, id, &gfx);
+        if (modelInfo->modelEntryForm) {
+            gfx = (uintptr_t)ModelEntry_getDisplayList(&modelInfo->modelEntryForm->modelEntry, id);
         }
     }
 
@@ -41,18 +44,20 @@ Mtx *ModelInfo_getMtx(ModelInfo *modelInfo, Link_EquipmentMatrix id) {
     uintptr_t mtx = 0;
 
     if (!recomputil_u32_value_hashmap_get(modelInfo->mtxOverrides, id, &mtx)) {
-        if (modelInfo->mtx) {
-            recomputil_u32_value_hashmap_get(modelInfo->mtx, id, &mtx);
+        if (modelInfo->modelEntryForm) {
+            mtx = (uintptr_t)ModelEntry_getMatrix(&modelInfo->modelEntryForm->modelEntry, id);
         }
     }
 
     return (Mtx *)mtx;
 }
 
-void ModelInfo_setEyesTextures(ModelInfo *modelInfo, TexturePtr eyeTextures[]) {
-    for (PlayerEyeIndex i = 0; i < PLAYER_EYES_MAX; ++i) {
-        modelInfo->eyesTextures[i] = eyeTextures[i];
-    }
+void ModelInfo_setModelEntryForm(ModelInfo *modelInfo, ModelEntryForm *modelEntry) {
+    modelInfo->modelEntryForm = modelEntry;
+}
+
+ModelEntryForm *ModelInfo_getModelEntryForm(ModelInfo *modelInfo) {
+    return modelInfo->modelEntryForm;
 }
 
 TexturePtr ModelInfo_getEyesTexture(ModelInfo *modelInfo, PlayerEyeIndex i) {
@@ -61,13 +66,11 @@ TexturePtr ModelInfo_getEyesTexture(ModelInfo *modelInfo, PlayerEyeIndex i) {
         return (TexturePtr)NULL;
     }
 
-    return modelInfo->eyesTextures[i];
-}
-
-void ModelInfo_setMouthTextures(ModelInfo *modelInfo, TexturePtr mouthTextures[]) {
-    for (PlayerMouthIndex i = 0; i < PLAYER_MOUTH_MAX; ++i) {
-        modelInfo->mouthTextures[i] = mouthTextures[i];
+    if (!modelInfo->modelEntryForm) {
+        return (TexturePtr)NULL;
     }
+
+    return ModelEntryForm_getEyesTexture(modelInfo->modelEntryForm, i);
 }
 
 TexturePtr ModelInfo_getMouthTexture(ModelInfo *modelInfo, PlayerMouthIndex i) {
@@ -76,31 +79,19 @@ TexturePtr ModelInfo_getMouthTexture(ModelInfo *modelInfo, PlayerMouthIndex i) {
         return (TexturePtr)NULL;
     }
 
-    return modelInfo->mouthTextures[i];
+    if (!modelInfo->modelEntryForm) {
+        return (TexturePtr)NULL;
+    }
+
+    return ModelEntryForm_getMouthTexture(modelInfo->modelEntryForm, i);
 }
 
-void ModelInfo_setSkeleton(ModelInfo *modelInfo, FlexSkeletonHeader *skel) {
-    modelInfo->skeleton = skel;
+FlexSkeletonHeader *ModelInfo_getSkeleton(ModelInfo *modelInfo) {
+    return modelInfo->modelEntryForm ? modelInfo->modelEntryForm->skel : NULL;
 }
 
-void ModelInfo_unsetSkeleton(ModelInfo *modelInfo) {
-    modelInfo->skeleton = NULL;
-}
-
-void ModelInfo_setShieldingSkeleton(ModelInfo *modelInfo, FlexSkeletonHeader *skel) {
-    modelInfo->shieldingSkeleton = skel;
-}
-
-void ModelInfo_unsetShieldingSkeleton(ModelInfo *modelInfo) {
-    modelInfo->shieldingSkeleton = NULL;
-}
-
-void ModelInfo_setGfxMap(ModelInfo *modelInfo, U32ValueHashmapHandle gfxMap) {
-    modelInfo->gfx = gfxMap;
-}
-
-void ModelInfo_unsetGfxMap(ModelInfo *modelInfo) {
-    modelInfo->gfx = 0;
+FlexSkeletonHeader *ModelInfo_getShieldingSkeleton(ModelInfo *modelInfo) {
+    return modelInfo->modelEntryForm ? modelInfo->modelEntryForm->shieldingSkel : NULL;
 }
 
 bool ModelInfo_setGfxOverride(ModelInfo *modelInfo, Link_DisplayList id, Gfx *dl) {
@@ -111,14 +102,6 @@ bool ModelInfo_unsetGfxOverride(ModelInfo *modelInfo, Link_DisplayList id) {
     return recomputil_u32_value_hashmap_erase(modelInfo->gfxOverrides, id);
 }
 
-void ModelInfo_setMtxMap(ModelInfo *modelInfo, U32ValueHashmapHandle mtxMap) {
-    modelInfo->mtx = mtxMap;
-}
-
-void ModelInfo_unsetMtxMap(ModelInfo *modelInfo) {
-    modelInfo->mtx = 0;
-}
-
 bool ModelInfo_setMtxOverride(ModelInfo *modelInfo, Link_EquipmentMatrix id, Mtx *mtx) {
     return recomputil_u32_value_hashmap_insert(modelInfo->mtxOverrides, id, (uintptr_t)mtx);
 }
@@ -127,29 +110,9 @@ bool ModelInfo_unsetMtxOverride(ModelInfo *modelInfo, Link_EquipmentMatrix id) {
     return recomputil_u32_value_hashmap_erase(modelInfo->mtxOverrides, id);
 }
 
-void ModelInfo_setFlags(ModelInfo *modelInfo, u64 flags) {
-    modelInfo->flags |= flags;
-}
-
-void ModelInfo_unsetFlags(ModelInfo *modelInfo, u64 flags) {
-    modelInfo->flags &= (~flags);
-}
-
-void ModelInfo_clearAllGfx(ModelInfo *modelInfo) {
-    for (Link_DisplayList i = 0; i < LINK_DL_MAX; ++i) {
-        ModelInfo_unsetGfx(modelInfo, i);
-    }
-}
-
 void ModelInfo_clearAllGfxOverrides(ModelInfo *modelInfo) {
     for (Link_DisplayList i = 0; i < LINK_DL_MAX; ++i) {
         ModelInfo_unsetGfxOverride(modelInfo, i);
-    }
-}
-
-void ModelInfo_clearAllMtx(ModelInfo *modelInfo) {
-    for (Link_EquipmentMatrix i = 0; i < LINK_EQUIP_MATRIX_MAX; ++i) {
-        ModelInfo_unsetMtx(modelInfo, i);
     }
 }
 
@@ -159,22 +122,6 @@ void ModelInfo_clearAllMtxOverrides(ModelInfo *modelInfo) {
     }
 }
 
-void ModelInfo_clearAllEyesTextures(ModelInfo *modelInfo) {
-    for (PlayerEyeIndex i = 0; i < PLAYER_EYES_MAX; ++i) {
-        modelInfo->eyesTextures[i] = 0;
-    }
-}
-
-void ModelInfo_clearAllMouthTextures(ModelInfo *modelInfo) {
-    for (PlayerMouthIndex i = 0; i < PLAYER_MOUTH_MAX; ++i) {
-        modelInfo->mouthTextures[i] = 0;
-    }
-}
-
-void ModelInfo_clearAllFlags(ModelInfo *modelInfo) {
-    modelInfo->flags = 0;
-}
-
 bool ModelInfo_isAnyFlagEnabled(ModelInfo *modelInfo, u64 flags) {
-    return modelInfo->flags & flags;
+    return modelInfo->modelEntryForm ? (modelInfo->modelEntryForm->modelEntry.flags & flags) : false;
 }
