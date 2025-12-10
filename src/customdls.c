6540c78f830c06b2f3baa75f90d46d6b37b3287c
Neirn <neirny@gmail.com>
2025-12-09 20:59:20 -0800

    Move certain custom DLs to own file + generate at runtime

---
+++ src/customdls.c
@@ -0,0 +1,104 @@
+#include "global.h"
+#include "modding.h"
+#include "customdls.h"
+#include "globalobjects_api.h"
+#include "logger.h"
+#include "assets/objects/object_link_child/object_link_child.h"
+
+#define ARRAY_LAST_ELEMENT_PTR(arr) (arr + (ARRAY_COUNT(arr) - 1))
+
+// handless ocarina DL
+Gfx gLinkHumanOcarinaDL[] = {
+    gsSPEndDisplayList(), // branch to copy of gLinkHumanRightHandHoldingOcarinaDL
+};
+
+// ocarina-less hand DL
+Gfx gLinkHumanRightHandOcarinaDL[] = {
+    gsDPPipeSync(),
+    gsDPSetTextureLUT(G_TT_NONE),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, 1, COMBINED, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED),
+    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
+    gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),
+    gsSPSetGeometryMode(G_CULL_BACK | G_FOG | G_LIGHTING),
+    gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
+    gsSPEndDisplayList(), // branch to middle of gLinkHumanRightHandHoldingOcarinaDL
+};
+
+// weaponless first person arm DL
+Gfx gLinkHumanFirstPersonArmDL[] = {
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPPipeSync(),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, TEXEL0, PRIMITIVE, 0, COMBINED, 0, 0, 0, 0, COMBINED),
+    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
+    gsDPPipeSync(),
+    gsDPSetTextureLUT(G_TT_RGBA16),
+    gsDPSetPrimColor(0, 0x80, 255, 255, 255, 255),
+    gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BACK | G_FOG | G_LIGHTING | G_SHADING_SMOOTH),
+    gsDPPipeSync(),
+    gsSPEndDisplayList(), // branch to first person arm here
+};
+
+// handless first person hookshot
+Gfx gLinkHumanFirstPersonHookshotDL[] = {
+    gsSPEndDisplayList(), // branch to first person hookshot DL
+};
+
+// handless first person bow
+Gfx gLinkHumanFirstPersonBowDL[] = {
+    gsSPEndDisplayList(), // branch to bow DL
+};
+
+static Gfx *getDLCopy(Gfx dl[], size_t n) {
+    size_t copySize = sizeof(Gfx) * n;
+    Gfx *copy = recomp_alloc(sizeof(*copy) * n);
+
+    Lib_MemCpy(copy, dl, copySize);
+
+    return copy;
+}
+
+static void unglueOcarinaFromHand() {
+    const size_t RIGHT_HAND_HOLDING_OCARINA_DL_LEN = 74;
+
+    Gfx *humanRightHandHoldingOcarinaDL = getDLCopy(GlobalObjects_getGlobalGfxPtr(OBJECT_LINK_CHILD, gLinkHumanRightHandHoldingOcarinaDL), RIGHT_HAND_HOLDING_OCARINA_DL_LEN);
+
+    const size_t OCARINA_START_DRAW_INDEX = 44;
+
+    gSPEndDisplayList(&humanRightHandHoldingOcarinaDL[OCARINA_START_DRAW_INDEX]);
+
+    gSPBranchList(gLinkHumanRightHandOcarinaDL, humanRightHandHoldingOcarinaDL);
+
+    gSPBranchList(ARRAY_LAST_ELEMENT_PTR(gLinkHumanOcarinaDL), &humanRightHandHoldingOcarinaDL[OCARINA_START_DRAW_INDEX + 1]);
+}
+
+static void unglueFirstPersonHandFromHookshot() {
+    const size_t RIGHT_HAND_HOLDING_HOOKSHOT_FIRST_PERSON_DL_LEN = 209;
+
+    Gfx *humanRightHandHoldingHookshotFirstPersonDL = getDLCopy(GlobalObjects_getGlobalGfxPtr(OBJECT_LINK_CHILD, object_link_child_DL_017B40), RIGHT_HAND_HOLDING_HOOKSHOT_FIRST_PERSON_DL_LEN);
+
+    const size_t FPS_RIGHT_HAND_START_DRAW_INDEX = 112;
+
+    gSPEndDisplayList(&humanRightHandHoldingHookshotFirstPersonDL[FPS_RIGHT_HAND_START_DRAW_INDEX]);
+
+    gSPBranchList(gLinkHumanFirstPersonHookshotDL, humanRightHandHoldingHookshotFirstPersonDL);
+
+    gSPBranchList(ARRAY_LAST_ELEMENT_PTR(gLinkHumanFirstPersonArmDL), &humanRightHandHoldingHookshotFirstPersonDL[FPS_RIGHT_HAND_START_DRAW_INDEX + 1]);
+}
+
+static void unglueFirstPersonBowFromHand() {
+    const size_t FPS_BOW_DL_LEN = 86;
+
+    Gfx *firstPersonBow = getDLCopy(GlobalObjects_getGlobalGfxPtr(OBJECT_LINK_CHILD, object_link_child_DL_018490), FPS_BOW_DL_LEN);
+
+    gSPEndDisplayList(&firstPersonBow[FPS_BOW_DL_LEN - 1]);
+
+    gSPBranchList(gLinkHumanFirstPersonBowDL, firstPersonBow);
+}
+
+GLOBAL_OBJECTS_CALLBACK_ON_READY
+void setupCustomDLs() {
+    unglueOcarinaFromHand();
+    unglueFirstPersonHandFromHookshot();
+    unglueFirstPersonBowFromHand();
+}
--- src/model_human.c
+++ src/model_human.c
@@ -9,184 +9,10 @@
 #include "modelmatrixids.h"
 #include "playermodelmanager_api.h"
 #include "defaultfacetex.h"
+#include "customdls.h"
 
 ModelEntryForm *gHumanModelEntry;
 ModelInfo gHumanModelInfo;
-
-// handless ocarina
-Gfx gLinkHumanOcarinaDL[] = {
-    gsDPPipeSync(),
-    gsDPSetTextureLUT(G_TT_RGBA16),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
-    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, 1, COMBINED, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED),
-    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
-    gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),
-    gsSPSetGeometryMode(G_CULL_BACK | G_FOG | G_LIGHTING),
-    gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
-    gsDPPipeSync(),
-    gsDPSetTextureLUT(G_TT_NONE),
-    gsDPLoadTextureBlock(gLinkHumanOcarinaTex, G_IM_FMT_RGBA, G_IM_SIZ_16b, 32, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP,
-                         G_TX_NOMIRROR | G_TX_CLAMP, 5, 4, G_TX_NOLOD, G_TX_NOLOD),
-    gsSPVertex(&object_link_childVtx_00E858[53], 23, 0),
-    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
-    gsSP2Triangles(4, 3, 2, 0, 4, 2, 1, 0),
-    gsSP2Triangles(5, 6, 7, 0, 8, 9, 10, 0),
-    gsSP2Triangles(10, 6, 8, 0, 5, 8, 6, 0),
-    gsSP2Triangles(11, 9, 0, 0, 10, 9, 11, 0),
-    gsSP2Triangles(12, 1, 13, 0, 11, 3, 14, 0),
-    gsSP2Triangles(11, 0, 3, 0, 0, 9, 1, 0),
-    gsSP2Triangles(12, 4, 1, 0, 1, 9, 8, 0),
-    gsSP2Triangles(1, 8, 5, 0, 14, 10, 11, 0),
-    gsSP2Triangles(1, 5, 13, 0, 15, 13, 5, 0),
-    gsSP2Triangles(15, 5, 7, 0, 7, 14, 15, 0),
-    gsSP2Triangles(10, 14, 6, 0, 6, 14, 7, 0),
-    gsSP2Triangles(12, 13, 16, 0, 12, 16, 17, 0),
-    gsSP2Triangles(17, 18, 15, 0, 15, 16, 13, 0),
-    gsSP2Triangles(17, 15, 14, 0, 17, 14, 19, 0),
-    gsSP2Triangles(14, 20, 21, 0, 14, 21, 19, 0),
-    gsSP2Triangles(4, 20, 3, 0, 3, 20, 14, 0),
-    gsSP2Triangles(12, 17, 19, 0, 12, 19, 22, 0),
-    gsSP2Triangles(12, 22, 4, 0, 21, 20, 4, 0),
-    gsSPEndDisplayList(),
-};
-
-// ocarina-less hand
-Gfx gLinkHumanRightHandOcarinaDL[] = {
-    gsDPPipeSync(),
-    gsDPSetTextureLUT(G_TT_RGBA16),
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
-    gsDPLoadTextureBlock(object_link_child_Tex_00DB08, G_IM_FMT_CI, G_IM_SIZ_8b, 16, 16, 0, G_TX_NOMIRROR | G_TX_CLAMP,
-                         G_TX_NOMIRROR | G_TX_CLAMP, 4, 4, G_TX_NOLOD, G_TX_NOLOD),
-    gsDPLoadTLUT_pal256(object_link_child_TLUT_00DA80),
-    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, 1, COMBINED, 0, PRIMITIVE, 0, 0, 0, 0, COMBINED),
-    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
-    gsSPClearGeometryMode(G_TEXTURE_GEN | G_TEXTURE_GEN_LINEAR),
-    gsSPSetGeometryMode(G_CULL_BACK | G_FOG | G_LIGHTING),
-    gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
-    gsSPVertex(object_link_childVtx_00E858, 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(2, 1, 6, 0, 7, 5, 4, 0),
-    gsSP2Triangles(8, 0, 2, 0, 6, 1, 0, 0),
-    gsSP2Triangles(9, 8, 2, 0, 10, 2, 6, 0),
-    gsSP2Triangles(2, 10, 9, 0, 0, 9, 11, 0),
-    gsSP2Triangles(12, 13, 14, 0, 15, 12, 14, 0),
-    gsSP2Triangles(15, 16, 17, 0, 14, 16, 15, 0),
-    gsSP2Triangles(0, 8, 9, 0, 14, 18, 16, 0),
-    gsSP2Triangles(16, 19, 17, 0, 20, 21, 5, 0),
-    gsSP2Triangles(5, 7, 20, 0, 22, 4, 3, 0),
-    gsSP2Triangles(23, 24, 25, 0, 17, 19, 13, 0),
-    gsSP2Triangles(25, 26, 23, 0, 27, 28, 29, 0),
-    gsSPVertex(&object_link_childVtx_00E858[30], 23, 0),
-    gsSP2Triangles(0, 1, 2, 0, 2, 1, 3, 0),
-    gsSP2Triangles(4, 5, 6, 0, 7, 8, 9, 0),
-    gsSP2Triangles(10, 11, 12, 0, 13, 9, 8, 0),
-    gsSP2Triangles(12, 14, 10, 0, 15, 16, 17, 0),
-    gsSP2Triangles(17, 12, 11, 0, 18, 11, 10, 0),
-    gsSP2Triangles(19, 3, 20, 0, 20, 1, 0, 0),
-    gsSP2Triangles(3, 19, 2, 0, 19, 21, 22, 0),
-    gsSP2Triangles(22, 2, 19, 0, 2, 22, 0, 0),
-    gsSP2Triangles(20, 0, 21, 0, 0, 22, 21, 0),
-    gsSPEndDisplayList(),
-};
-
-// first person arm
-Gfx gLinkHumanFirstPersonArmDL[] = {
-    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
-    gsDPPipeSync(),
-    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, TEXEL0, PRIMITIVE, 0, COMBINED, 0, 0, 0, 0, COMBINED),
-    gsDPSetRenderMode(G_RM_FOG_SHADE_A, G_RM_AA_ZB_OPA_SURF2),
-    gsDPPipeSync(),
-    gsDPSetTextureLUT(G_TT_RGBA16),
-    gsDPSetPrimColor(0, 0x80, 255, 255, 255, 255),
-    gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BACK | G_FOG | G_LIGHTING | G_SHADING_SMOOTH),
-    gsDPPipeSync(),
-    gsDPLoadTLUT_pal256(object_link_child_TLUT_018A40),
-    gsDPLoadTextureBlock(object_link_child_Tex_019240, G_IM_FMT_CI, G_IM_SIZ_8b, 8, 8, 0, G_TX_MIRROR | G_TX_CLAMP,
-                         G_TX_MIRROR | G_TX_CLAMP, 3, 3, G_TX_NOLOD, G_TX_NOLOD),
-    gsSPVertex(&object_link_childVtx_010538[607], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(6, 2, 1, 0, 7, 2, 8, 0),
-    gsSP2Triangles(7, 8, 9, 0, 10, 11, 12, 0),
-    gsSP2Triangles(13, 14, 15, 0, 16, 17, 18, 0),
-    gsSP2Triangles(19, 20, 21, 0, 22, 23, 24, 0),
-    gsSP2Triangles(25, 20, 26, 0, 27, 28, 29, 0),
-    gsSP1Triangle(27, 25, 30, 0),
-    gsSPVertex(&object_link_childVtx_010538[638], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 1, 0, 3, 0),
-    gsSP2Triangles(4, 5, 6, 0, 7, 8, 9, 0),
-    gsSP2Triangles(7, 9, 10, 0, 11, 7, 12, 0),
-    gsSP2Triangles(13, 14, 12, 0, 10, 9, 15, 0),
-    gsSP2Triangles(10, 15, 6, 0, 16, 8, 17, 0),
-    gsSP2Triangles(18, 19, 20, 0, 21, 18, 16, 0),
-    gsSP2Triangles(18, 20, 22, 0, 23, 24, 16, 0),
-    gsSP2Triangles(3, 0, 25, 0, 26, 21, 27, 0),
-    gsSP2Triangles(26, 27, 28, 0, 24, 29, 30, 0),
-    gsSP1Triangle(25, 0, 31, 0),
-    gsSPVertex(&object_link_childVtx_010538[670], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
-    gsSP2Triangles(4, 5, 6, 0, 7, 8, 9, 0),
-    gsSP2Triangles(10, 11, 1, 0, 12, 13, 14, 0),
-    gsSP2Triangles(3, 2, 15, 0, 3, 15, 16, 0),
-    gsSP2Triangles(17, 18, 19, 0, 20, 21, 22, 0),
-    gsSP2Triangles(23, 24, 25, 0, 23, 26, 24, 0),
-    gsSP2Triangles(23, 21, 20, 0, 27, 28, 29, 0),
-    gsSP2Triangles(23, 25, 30, 0, 30, 21, 23, 0),
-    gsSPVertex(&object_link_childVtx_010538[701], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(6, 7, 8, 0, 9, 7, 6, 0),
-    gsSP2Triangles(10, 11, 12, 0, 13, 14, 12, 0),
-    gsSP2Triangles(15, 0, 6, 0, 1, 0, 15, 0),
-    gsSP2Triangles(16, 17, 1, 0, 15, 5, 18, 0),
-    gsSP2Triangles(2, 17, 14, 0, 6, 19, 15, 0),
-    gsSP2Triangles(15, 18, 1, 0, 20, 21, 22, 0),
-    gsSP2Triangles(23, 24, 8, 0, 14, 17, 25, 0),
-    gsSP2Triangles(26, 27, 28, 0, 11, 10, 29, 0),
-    gsSP1Triangle(12, 30, 10, 0),
-    gsSPVertex(&object_link_childVtx_010538[732], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(3, 6, 4, 0, 6, 7, 4, 0),
-    gsSP2Triangles(0, 8, 9, 0, 10, 11, 12, 0),
-    gsSP2Triangles(0, 9, 13, 0, 3, 14, 15, 0),
-    gsSP2Triangles(16, 17, 18, 0, 19, 20, 21, 0),
-    gsSP2Triangles(22, 14, 23, 0, 3, 23, 14, 0),
-    gsSP2Triangles(24, 25, 26, 0, 24, 26, 27, 0),
-    gsSP2Triangles(28, 29, 30, 0, 27, 14, 22, 0),
-    gsSPVertex(&object_link_childVtx_010538[763], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(3, 5, 6, 0, 2, 7, 0, 0),
-    gsSP2Triangles(2, 1, 8, 0, 0, 9, 10, 0),
-    gsSP2Triangles(2, 11, 12, 0, 13, 14, 15, 0),
-    gsSP2Triangles(16, 17, 18, 0, 3, 19, 20, 0),
-    gsSP2Triangles(4, 21, 22, 0, 21, 23, 22, 0),
-    gsSP2Triangles(12, 7, 2, 0, 24, 25, 8, 0),
-    gsSP2Triangles(23, 26, 27, 0, 0, 10, 28, 0),
-    gsSP1Triangle(29, 9, 30, 0),
-    gsSPVertex(&object_link_childVtx_010538[794], 32, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(3, 5, 6, 0, 7, 8, 9, 0),
-    gsSP2Triangles(10, 11, 12, 0, 1, 13, 14, 0),
-    gsSP2Triangles(15, 16, 17, 0, 16, 15, 18, 0),
-    gsSP2Triangles(18, 15, 19, 0, 20, 21, 17, 0),
-    gsSP2Triangles(20, 22, 21, 0, 23, 24, 25, 0),
-    gsSP2Triangles(26, 24, 23, 0, 20, 27, 22, 0),
-    gsSP2Triangles(18, 28, 29, 0, 18, 30, 28, 0),
-    gsSPVertex(&object_link_childVtx_010538[825], 31, 0),
-    gsSP2Triangles(0, 1, 2, 0, 3, 4, 5, 0),
-    gsSP2Triangles(6, 0, 2, 0, 7, 3, 5, 0),
-    gsSP2Triangles(8, 9, 4, 0, 6, 2, 10, 0),
-    gsSP2Triangles(11, 10, 9, 0, 12, 13, 7, 0),
-    gsSP2Triangles(7, 5, 12, 0, 14, 13, 12, 0),
-    gsSP2Triangles(15, 16, 8, 0, 17, 3, 7, 0),
-    gsSP2Triangles(18, 17, 7, 0, 17, 15, 3, 0),
-    gsSP2Triangles(19, 20, 21, 0, 13, 20, 19, 0),
-    gsSP2Triangles(20, 0, 21, 0, 14, 20, 13, 0),
-    gsSP2Triangles(20, 1, 0, 0, 14, 1, 20, 0),
-    gsSP2Triangles(6, 11, 22, 0, 6, 10, 11, 0),
-    gsSP2Triangles(23, 24, 25, 0, 24, 26, 25, 0),
-    gsSP2Triangles(23, 27, 24, 0, 23, 28, 27, 0),
-    gsSP2Triangles(28, 29, 27, 0, 29, 30, 27, 0),
-    gsSPEndDisplayList(),
-};
 
 Gfx *getHumanDL(Gfx *dl) {
     return GlobalObjects_getGlobalGfxPtr(OBJECT_LINK_CHILD, dl);
@@ -244,11 +70,6 @@ static void setupHumanFallbackModel() {
 
     SET_ENTRY_DL(LINK_DL_RFIST, getHumanDL(gLinkHumanRightHandClosedDL));
 
-    GlobalObjectsSegmentMap segments = {0};
-    segments[4] = GlobalObjects_getGlobalObject(GAMEPLAY_KEEP);
-    segments[6] = human;
-
-    GlobalObjects_rebaseDL(gLinkHumanRightHandOcarinaDL, segments); // repoint vertices, textures, etc. to static link obj
     SET_ENTRY_DL(LINK_DL_OPT_RHAND_OCARINA, gLinkHumanRightHandOcarinaDL);
 
     // First Person
@@ -259,11 +80,9 @@ static void setupHumanFallbackModel() {
     SET_ENTRY_DL(LINK_DL_OPT_FPS_LFOREARM_SLINGSHOT, gEmptyDL);
     SET_ENTRY_DL(LINK_DL_OPT_FPS_LHAND_SLINGSHOT, gEmptyDL);
 
-    GlobalObjects_rebaseDL(gLinkHumanFirstPersonArmDL, segments); // repoint vertices, textures, etc. to static link obj
     SET_ENTRY_DL(LINK_DL_FPS_RHAND, gLinkHumanFirstPersonArmDL);
 
     // items
-    GlobalObjects_rebaseDL(gLinkHumanOcarinaDL, segments); // repoint vertices, textures, etc. to static link obj
     SET_ENTRY_DL(LINK_DL_OCARINA_TIME, gLinkHumanOcarinaDL);
 
 #undef SET_ENTRY_DL
