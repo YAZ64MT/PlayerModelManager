YAZ64MT/PlayerModelManager

commit 37f947beaa48ce5179988b9f15edb9f29ce0c8a2

Simplify model refreshes
Granularity = more places for bugs to sneak in!

Also easier to follow the logic

Re-added queue system to do any refreshes at the start of a frame

src/formproxy.c
@@ -884,7 +884,7 @@ void FormProxy_setCurrentModelFormEntry(FormProxy *fp, ModelEntryForm *modelEntr
     ModelInfo_setModelEntryForm(&fp->currentModelInfo, modelEntry);
 }
 
-void FormProxy_refreshPlayerFaceTextures(FormProxy *fp) {
+void FormProxy_repointPlayerFaceTexturePtrs(FormProxy *fp) {
     if (!isValidFormProxy(fp)) {
         PRINT_INVALID_PTR_ERR();
         return;
@@ -926,7 +926,7 @@ void FormProxy_refreshPlayerFaceTextures(FormProxy *fp) {
     }
 }
 
-void FormProxy_refresh(FormProxy *fp) {
+static void FormProxy_refresh(FormProxy *fp) {
     if (!isValidFormProxy(fp)) {
         PRINT_INVALID_PTR_ERR();
         return;
@@ -1127,7 +1127,8 @@ Gfx *FormProxy_getDL(FormProxy *fp, Link_DisplayList id) {
 
     if (!dl) {
         FormProxy *fpAlt;
-        if (recomputil_u32_value_hashmap_get(fp->displayListAlternates, id, (uintptr_t *)&fpAlt)) {
+        recomputil_u32_value_hashmap_get(fp->displayListAlternates, id, (uintptr_t *)&fpAlt);
+        if (fpAlt) {
             if (fpAlt && fpAlt != fp) {
                 dl = FormProxy_getDL(fpAlt, id);
             }
@@ -1221,6 +1222,35 @@ PlayerTransformation FormProxy_getTargetForm(FormProxy *fp) {
     return fp->form;
 }
 
+PlayerModelManagerModelType FormProxy_getModelType(FormProxy *fp) {
+    if (!isValidFormProxy(fp)) {
+        PRINT_INVALID_PTR_ERR();
+        return PMM_MODEL_TYPE_NONE;
+    }
+
+    ModelEntryForm *entry = ModelInfo_getModelEntryForm(&fp->currentModelInfo);
+
+    if (!entry) {
+        entry = ModelInfo_getModelEntryForm(fp->fallbackOverrideModelInfo);
+    }
+
+    if (!entry) {
+        entry = ModelInfo_getModelEntryForm(fp->fallbackModelInfo);
+    }
+
+    if (entry) {
+        return ModelEntry_getType(ModelEntryForm_getModelEntry(entry));
+    }
+
+    return PMM_MODEL_TYPE_NONE;
+}
+
+static YAZMTCore_IterableU32Set *sQueuedRefreshes;
+
+void FormProxy_requestRefresh(FormProxy *fp) {
+    YAZMTCore_IterableU32Set_insert(sQueuedRefreshes, (uintptr_t)fp);
+}
+
 static void setupAltLists() {
     sDLToAltLists = recomputil_create_u32_value_hashmap();
 
@@ -1237,4 +1267,22 @@ RECOMP_CALLBACK(".", _internal_initHashObjects)
 void initFormProxyObjects() {
     sValidFormProxyPtrs = recomputil_create_u32_hashset();
     setupAltLists();
+    sQueuedRefreshes = YAZMTCore_IterableU32Set_new();
+}
+
+RECOMP_HOOK("Play_Main")
+void processRefreshRequests_on_Play_Main(GameState *thisx) {
+    size_t numRequests = YAZMTCore_IterableU32Set_size(sQueuedRefreshes);
+
+    if (numRequests > 0) {
+        FormProxy **proxies = (FormProxy **)YAZMTCore_IterableU32Set_values(sQueuedRefreshes);
+
+        for (size_t i = 0; i < numRequests; ++i) {
+            FormProxy *curr = proxies[i];
+
+            FormProxy_refresh(curr);
+        }
+
+        YAZMTCore_IterableU32Set_clear(sQueuedRefreshes);
+    }
 }

include/formproxy.h
@@ -4,6 +4,7 @@
 #include "modelmatrixids.h"
 #include "modelinfo.h"
 #include "z64recomp_api.h"
+#include "playermodelmanager_api.h"
 
 typedef struct PlayerProxy PlayerProxy;
 
@@ -65,11 +66,6 @@ bool FormProxy_setCurrentOverrideMtx(FormProxy *fp, Link_EquipmentMatrix mtxId, 
 bool FormProxy_unsetCurrentOverrideMtx(FormProxy *fp, Link_EquipmentMatrix mtxId);
 bool FormProxy_setAlternateFormProxyDL(FormProxy *fp, Link_DisplayList id, FormProxy *alt);
 bool FormProxy_unsetAlternateFormProxyDL(FormProxy *fp, Link_DisplayList id);
-void FormProxy_refreshSkeletons(FormProxy *fp);
-void FormProxy_refreshDL(FormProxy *fp, Link_DisplayList id);
-void FormProxy_refreshAllDLs(FormProxy *fp);
-void FormProxy_refreshMtx(FormProxy *fp, Link_EquipmentMatrix id);
-void FormProxy_refreshAllMtxs(FormProxy *fp);
 void FormProxy_requestTunicColorOverride(FormProxy *fp, Color_RGBA8 color);
 bool FormProxy_isTunicColorOverrideRequested(FormProxy *fp);
 Color_RGBA8 FormProxy_getRequestedTunicColor(FormProxy *fp);
@@ -78,8 +74,8 @@ void FormProxy_resetTunicColor(FormProxy *fp);
 void FormProxy_pullCurrentTunicColorFromConfig(FormProxy *fp);
 void FormProxy_pullCurrentTunicColorFromRequested(FormProxy *fp);
 void FormProxy_setCurrentModelFormEntry(FormProxy *fp, ModelEntryForm *modelEntry);
-void FormProxy_refreshPlayerFaceTextures(FormProxy *fp);
-void FormProxy_refresh(FormProxy *fp);
+void FormProxy_repointPlayerFaceTexturePtrs(FormProxy *fp);
+void FormProxy_requestRefresh(FormProxy *fp);
 Mtx *FormProxy_getMatrix(FormProxy *fp, Link_EquipmentMatrix id);
 Gfx *FormProxy_getDL(FormProxy *fp, Link_DisplayList id);
 Gfx *FormProxy_getCurrentDL(FormProxy *fp, Link_DisplayList id);
@@ -88,5 +84,6 @@ FlexSkeletonHeader *FormProxy_getShieldingSkeleton(FormProxy *fp);
 bool FormProxy_isAdultModelType(FormProxy *fp);
 PlayerProxy *FormProxy_getPlayerProxy(FormProxy *fp);
 PlayerTransformation FormProxy_getTargetForm(FormProxy *fp);
+PlayerModelManagerModelType FormProxy_getModelType(FormProxy *fp);
 
 #endif

include/playerproxy.h
@@ -17,10 +17,7 @@ typedef struct PlayerProxy {
 void PlayerProxy_init(PlayerProxy *pp);
 FormProxy *PlayerProxy_getFormProxy(PlayerProxy *pp, FormProxyId formId);
 void PlayerProxy_refresh(PlayerProxy *pp);
-void PlayerProxy_refreshAlternateDLs(PlayerProxy *pp);
-void PlayerProxy_refreshDL(PlayerProxy *pp, Link_DisplayList dlId);
 void PlayerProxy_setOverrideDL(PlayerProxy *pp, Link_DisplayList dlId, Gfx *dl);
-void PlayerProxy_refreshMtx(PlayerProxy *pp, Link_EquipmentMatrix mtxId);
 void PlayerProxy_setOverrideMtx(PlayerProxy *pp, Link_EquipmentMatrix mtxId, Mtx *mtx);
 bool PlayerProxy_getProxyIdFromForm(PlayerTransformation form, FormProxyId *out);
 void PlayerProxy_requestTunicColorOverride(PlayerProxy *pp, Color_RGBA8 color);

include/playerproxymanager.h
@@ -9,7 +9,5 @@ typedef struct ModelEntry ModelEntry;
 PlayerProxy *PlayerProxyManager_createPlayerProxy();
 void PlayerProxyManager_refreshAll();
 void PlayerProxyManager_refreshFullAllWithModelEntry(ModelEntry *modelEntry);
-void PlayerProxyManager_refreshDLAll(Link_DisplayList dlId);
-void PlayerProxyManager_refreshMtxAll(Link_EquipmentMatrix mtxId);
 
 #endif

src/api.c
@@ -547,7 +547,7 @@ RECOMP_EXPORT bool PlayerModelManager_overrideVanillaDisplayList(unsigned long a
 
             if (fallbackModelInfo) {
                 ModelInfo_setGfxOverride(fallbackModelInfo, dlId, dl);
-                PlayerProxyManager_refreshDLAll(dlId);
+                PlayerProxyManager_refreshAll();
 
                 // preserve model behavior of old API versions
                 if (form == PLAYER_FORM_HUMAN) {
@@ -614,7 +614,7 @@ RECOMP_EXPORT bool PlayerModelManager_overrideVanillaMatrix(unsigned long apiVer
 
             if (fallbackModelInfo) {
                 ModelInfo_setMtxOverride(fallbackModelInfo, mtxId, mtx);
-                PlayerProxyManager_refreshMtxAll(mtxId);
+                PlayerProxyManager_refreshAll();
             }
         }
     }

src/main.c
@@ -214,7 +214,7 @@ void updateAssets_on_Player_Draw(Actor *thisx, PlayState *play) {
 
     if (fp) {
         Player *player = (Player *)thisx;
-        FormProxy_refreshPlayerFaceTextures(fp);
+        FormProxy_repointPlayerFaceTexturePtrs(fp);
         repointSharedModelsToProxy(fp);
         repointFormPtrsToProxy(player, fp);
         FlexSkeletonHeader *skel = FormProxy_getSkeleton(fp);

src/modelentry.c
@@ -10,6 +10,7 @@
 #include "logger.h"
 #include "defaultfacetex.h"
 #include "libc/string.h"
+#include "playermodelconfig.h"
 
 #define PRINT_INVALID_PTR_ERR() Logger_printError("Received invalid pointer.")
 
@@ -113,7 +114,7 @@ typedef struct ModelEntryForm {
 typedef struct ModelEntryEquipment {
     ModelEntry modelEntry;
     ModelVisuals fallbackModelVisuals;
-    U32ValueHashmapHandle formModelVisuals;
+    U32ValueHashmapHandle modelTypeToModelVisuals;
     Link_EquipmentReplacement equipType;
 } ModelEntryEquipment;
 
@@ -260,8 +261,7 @@ static bool ModelEntryForm_applyToFormProxy(const ModelEntry *thisx, FormProxy *
     if (pp) {
         FormProxy_setCurrentModelFormEntry(fp, (ModelEntryForm *)thisx);
 
-        FormProxy_refresh(fp);
-        PlayerProxy_refreshAlternateDLs(pp);
+        PlayerProxy_refresh(pp);
 
         _internal_onFormModelApplied(FormProxy_getTargetForm(fp));
 
@@ -277,8 +277,7 @@ static bool ModelEntryForm_removeFromFormProxy(const ModelEntry *thisx, FormProx
     if (pp) {
         FormProxy_setCurrentModelFormEntry(fp, NULL);
 
-        FormProxy_refresh(fp);
-        PlayerProxy_refreshAlternateDLs(pp);
+        PlayerProxy_refresh(pp);
 
         _internal_onFormModelApplied(FormProxy_getTargetForm(fp));
 
@@ -697,6 +696,47 @@ static Gfx *ModelEntryEquipment_getDisplayList(const ModelEntry *thisx, Link_Dis
     return ModelVisuals_getDL(&this->fallbackModelVisuals, id);
 }
 
+static ModelVisuals *ModelEntryEquipment_getModelTypeModelVisual(ModelEntryEquipment *entry, PlayerModelManagerModelType type) {
+    return recomputil_u32_memory_hashmap_get(entry->modelTypeToModelVisuals, type);
+}
+
+static ModelVisuals *ModelEntryEquipment_getOrCreateModelTypeModelVisual(ModelEntryEquipment *entry, PlayerModelManagerModelType type) {
+    ModelVisuals *modelVisuals = ModelEntryEquipment_getModelTypeModelVisual(entry, type);
+
+    if (!modelVisuals) {
+        recomputil_u32_memory_hashmap_create(entry->modelTypeToModelVisuals, type);
+        modelVisuals = recomputil_u32_memory_hashmap_get(entry->modelTypeToModelVisuals, type);
+        if (modelVisuals) {
+            ModelVisuals_init(modelVisuals);
+        }
+    }
+
+    return modelVisuals;
+}
+
+bool ModelEntryEquipment_setDisplayListForModelType(ModelEntryEquipment *entry, PlayerModelManagerModelType type, Link_DisplayList id, Gfx *dl) {
+    ModelVisuals *modelVisuals = ModelEntryEquipment_getOrCreateModelTypeModelVisual(entry, type);
+
+    if (modelVisuals) {
+        ModelVisuals_setDL(modelVisuals, id, dl);
+        return true;
+    }
+
+    return false;
+}
+
+static Gfx *ModelEntryEquipment_getDisplayListForModelType(ModelEntryEquipment *entry, PlayerModelManagerModelType type, Link_DisplayList id) {
+    Gfx *dl = NULL;
+
+    ModelVisuals *overrideModelVisuals = ModelEntryEquipment_getModelTypeModelVisual(entry, type);
+
+    if (overrideModelVisuals) {
+        dl = ModelVisuals_getDL(overrideModelVisuals, id);
+    }
+
+    return dl;
+}
+
 static bool ModelEntryEquipment_setMatrix(ModelEntry *thisx, Link_EquipmentMatrix id, Mtx *mtx) {
     ModelEntryEquipment *this = (ModelEntryEquipment *)((void *)thisx);
 
@@ -720,28 +760,71 @@ static Mtx *ModelEntryEquipment_getMatrix(const ModelEntry *thisx, Link_Equipmen
     return ModelVisuals_getMtx(&this->fallbackModelVisuals, id);
 }
 
+bool ModelEntryEquipment_setMatrixForModelType(ModelEntryEquipment *entry, PlayerModelManagerModelType type, Link_EquipmentMatrix id, Mtx *mtx) {
+    ModelVisuals *modelVisuals = ModelEntryEquipment_getOrCreateModelTypeModelVisual(entry, type);
+
+    if (modelVisuals) {
+        ModelVisuals_setMtx(modelVisuals, id, mtx);
+        return true;
+    }
+
+    return false;
+}
+
+Mtx *ModelEntryEquipment_getMatrixForModelType(ModelEntryEquipment *entry, PlayerModelManagerModelType type, Link_EquipmentMatrix id) {
+    Mtx *mtx = NULL;
+
+    ModelVisuals *overrideModelVisuals = ModelEntryEquipment_getModelTypeModelVisual(entry, type);
+
+    if (overrideModelVisuals) {
+        mtx = ModelVisuals_getMtx(overrideModelVisuals, id);
+    }
+
+    return mtx;
+}
+
 static bool ModelEntryEquipment_applyToFormProxy(const ModelEntry *thisx, FormProxy *fp) {
     PlayerProxy *pp = FormProxy_getPlayerProxy(fp);
 
     if (pp) {
+        FormProxyId maxFormIds = PlayerModelConfig_getNumFormIds();
+
         ModelEntryEquipment *this = (ModelEntryEquipment *)((void *)thisx);
 
         const EquipmentOverride *override = EquipmentOverrides_getEquipmentOverride(this->equipType);
 
         if (override) {
-            for (size_t i = 0; i < override->dl.count; ++i) {
-                Link_DisplayList id = override->dl.overrides[i];
-                Gfx *dl = ModelVisuals_getDL(&this->fallbackModelVisuals, id);
-                PlayerProxy_setOverrideDL(pp, id, dl);
-                PlayerProxy_refreshDL(pp, id);
+            for (FormProxyId i = 0; i < maxFormIds; i++) {
+                FormProxy *currFp = PlayerProxy_getFormProxy(pp, i);
+                if (currFp) {
+                    PlayerModelManagerModelType type = FormProxy_getModelType(currFp);
+
+                    for (size_t j = 0; j < override->dl.count; ++j) {
+                        Link_DisplayList id = override->dl.overrides[j];
+
+                        Gfx *allFormsDL = ModelVisuals_getDL(&this->fallbackModelVisuals, id);
+                        PlayerProxy_setOverrideDL(pp, id, allFormsDL);
+
+                        Gfx *singleFormDL = ModelEntryEquipment_getDisplayListForModelType(this, type, id);
+                        if (singleFormDL) {
+                            FormProxy_setCurrentOverrideDL(currFp, id, singleFormDL);
+                        }
+                    }
+
+                    for (size_t j = 0; j < override->mtx.count; ++j) {
+                        Link_EquipmentMatrix id = override->mtx.overrides[j];
+                        Mtx *allFormsMtx = ModelVisuals_getMtx(&this->fallbackModelVisuals, id);
+                        PlayerProxy_setOverrideMtx(pp, id, allFormsMtx);
+
+                        Mtx *singleFormMtx = ModelEntryEquipment_getMatrixForModelType(this, type, id);
+                        if (singleFormMtx) {
+                            FormProxy_setCurrentOverrideMtx(currFp, id, singleFormMtx);
+                        }
+                    }
+                }
             }
 
-            for (size_t i = 0; i < override->mtx.count; ++i) {
-                Link_EquipmentMatrix id = override->mtx.overrides[i];
-                Mtx *mtx = ModelVisuals_getMtx(&this->fallbackModelVisuals, id);
-                PlayerProxy_setOverrideMtx(pp, id, mtx);
-                PlayerProxy_refreshMtx(pp, id);
-            }
+            PlayerProxy_refresh(pp);
 
             _internal_onEquipmentModelApplied(this->equipType);
 
@@ -764,14 +847,14 @@ static bool ModelEntryEquipment_removeFromFormProxy(const ModelEntry *thisx, For
             for (size_t i = 0; i < override->dl.count; ++i) {
                 Link_DisplayList id = override->dl.overrides[i];
                 PlayerProxy_setOverrideDL(pp, id, NULL);
-                PlayerProxy_refreshDL(pp, id);
             }
 
             for (size_t i = 0; i < override->mtx.count; ++i) {
                 Link_EquipmentMatrix id = override->mtx.overrides[i];
                 PlayerProxy_setOverrideMtx(pp, id, NULL);
-                PlayerProxy_refreshMtx(pp, id);
             }
+
+            PlayerProxy_refresh(pp);
 
             _internal_onEquipmentModelApplied(this->equipType);
 
@@ -804,7 +887,7 @@ static bool ModelEntryEquipment_init(ModelEntryEquipment *entry, PlayerModelMana
 
     entry->modelEntry.virtualFuncs = &funcs;
     ModelVisuals_init(&entry->fallbackModelVisuals);
-    entry->formModelVisuals = recomputil_create_u32_memory_hashmap(sizeof(ModelVisuals));
+    entry->modelTypeToModelVisuals = recomputil_create_u32_memory_hashmap(sizeof(ModelVisuals));
     entry->equipType = getEquipmentReplacementFromCategory(entry->modelEntry.category);
 
     recomputil_u32_hashset_insert(sValidModelEntryEquipmentPtrs, (uintptr_t)entry);

src/playerproxy.c
@@ -115,38 +115,11 @@ void PlayerProxy_refresh(PlayerProxy *pp) {
         FormProxy *currFp = PlayerProxy_getFormProxy(pp, i);
 
         if (currFp) {
-            FormProxy_refresh(currFp);
+            FormProxy_requestRefresh(currFp);
         }
     }
 }
 
-void PlayerProxy_refreshAlternateDLs(PlayerProxy *pp) {
-    FormProxyId idMax = PlayerModelConfig_getNumFormIds();
-
-    for (int i = 0; i < ARRAY_COUNT(sFormAlternates); ++i) {
-        const FormAlternateDL *currAlt = &sFormAlternates[i];
-
-        for (FormProxyId j = 0; j < idMax; ++j) {
-            FormProxy *currFp = PlayerProxy_getFormProxy(pp, j);
-
-            if (currFp && currAlt->formProxyId != j) {
-                FormProxy_refreshDL(currFp, currAlt->formProxyId);
-            }
-        }
-    }
-}
-
-void PlayerProxy_refreshDL(PlayerProxy *pp, Link_DisplayList dlId) {
-    FormProxyId idMax = PlayerModelConfig_getNumFormIds();
-
-    for (FormProxyId i = 0; i < idMax; ++i) {
-        FormProxy *currFp = PlayerProxy_getFormProxy(pp, i);
-
-        if (currFp) {
-            FormProxy_refreshDL(currFp, dlId);
-        }
-    }
-}
 
 void PlayerProxy_setOverrideDL(PlayerProxy *pp, Link_DisplayList dlId, Gfx *dl) {
     FormProxyId idMax = PlayerModelConfig_getNumFormIds();
@@ -156,18 +129,6 @@ void PlayerProxy_setOverrideDL(PlayerProxy *pp, Link_DisplayList dlId, Gfx *dl) 
 
         if (currFp) {
             FormProxy_setCurrentOverrideDL(currFp, dlId, dl);
-        }
-    }
-}
-
-void PlayerProxy_refreshMtx(PlayerProxy *pp, Link_EquipmentMatrix mtxId) {
-    FormProxyId idMax = PlayerModelConfig_getNumFormIds();
-
-    for (FormProxyId i = 0; i < idMax; ++i) {
-        FormProxy *currFp = PlayerProxy_getFormProxy(pp, i);
-
-        if (currFp) {
-            FormProxy_refreshMtx(currFp, mtxId);
         }
     }
 }

commit c6dc65d63084039aa84bc0db97b575e1134f5bc4

Fix uninitialized pointer being passed in FormProxy_getDL
Fixes incorrect behavior introduced in 37f947b

src/formproxy.c
@@ -1126,7 +1126,7 @@ Gfx *FormProxy_getDL(FormProxy *fp, Link_DisplayList id) {
     Gfx *dl = MRC_getListenerDL(fp->form, id);
 
     if (!dl) {
-        FormProxy *fpAlt;
+        FormProxy *fpAlt = NULL;
         recomputil_u32_value_hashmap_get(fp->displayListAlternates, id, (uintptr_t *)&fpAlt);
         if (fpAlt) {
             if (fpAlt && fpAlt != fp) {