063efb37f5e00e1b6a2aa6120ea27e2a3e2b9338
Neirn <neirny@gmail.com>
2025-12-30 18:45:45 -0800

    Create fallbackmodels.h

---
+++ include/fallbackmodels.h
@@ -0,0 +1,12 @@
+#ifndef FALLBACKMODELS_H
+#define FALLBACKMODELS_H
+
+typedef struct ModelEntryForm ModelEntryForm;
+
+ModelEntryForm *gHumanModelEntry;
+ModelEntryForm *gDekuModelEntry;
+ModelEntryForm *gGoronModelEntry;
+ModelEntryForm *gZoraModelEntry;
+ModelEntryForm *gFierceDeityModelEntry;
+
+#endif
