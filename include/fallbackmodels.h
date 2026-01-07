a97d23ca4429ecbbdf103232ddd75faf3e63991f
Neirn <neirny@gmail.com>
2026-01-06 23:10:31 -0800

    Global handles now externs in header

--- include/fallbackmodels.h
+++ include/fallbackmodels.h
@@ -6,13 +6,15 @@
 
 typedef struct ModelEntryForm ModelEntryForm;
 
-PlayerModelManagerHandle gHumanModelHandle;
-PlayerModelManagerHandle gDekuModelHandle;
-PlayerModelManagerHandle gGoronModelHandle;
-PlayerModelManagerHandle gZoraModelHandle;
-PlayerModelManagerHandle gFierceDeityModelHandle;
+extern PlayerModelManagerHandle gHumanModelHandle;
+extern PlayerModelManagerHandle gDekuModelHandle;
+extern PlayerModelManagerHandle gGoronModelHandle;
+extern PlayerModelManagerHandle gZoraModelHandle;
+extern PlayerModelManagerHandle gFierceDeityModelHandle;
 
 // FlexSkeletonHeader should contain a pointer to an array of LodLimb limbs
 void FallbackModelsCommon_doCommonAssignments(PlayerModelManagerHandle h, FlexSkeletonHeader *skel, void *seg06, void *seg04);
+void FallbackModelsCommon_addEquipmentAdultMM(PlayerModelManagerHandle h);
+void FallbackModelsCommon_addEquipmentChildMM(PlayerModelManagerHandle h);
 
 #endif
