d7aff994a529033a1acd225b40763599e9d7729f
Neirn <neirny@gmail.com>
2026-03-26 23:25:37 -0700

    Make human Link's tunic colorable

--- src/fallbackmodels/mm/human.c
+++ src/fallbackmodels/mm/human.c
@@ -1,5 +1,5 @@
-#include "modding.h"
 #include "global.h"
+#include "modding.h"
 #include "assets/objects/object_link_child/object_link_child.h"
 #include "utils.h"
 #include "globalobjects_api.h"
@@ -10,11 +10,37 @@
 #include "fallbackmodels.h"
 #include "logger.h"
 #include "mm/vanillawrapperdls.h"
+#include "yazmtcorelib_api.h"
 
 PlayerModelManagerHandle gHumanModelHandle;
 
+Gfx sPrimTunicCombinerDL[] = {
+    gsDPSetCombineLERP(TEXEL0, 0, PRIMITIVE, 0, 0, 0, 0, TEXEL0, COMBINED, 0, SHADE, 0, 0, 0, 0, COMBINED),
+    gsDPSetPrimColor(0, 0xFF, 30, 105, 27, 255),
+};
+
+Gfx sEnvTunicCombinerDL[] = {gsDPSetCombineLERP(TEXEL0, 0, ENVIRONMENT, 0, 0, 0, 0, TEXEL0, COMBINED, 0, SHADE, 0, 0, 0, 0, COMBINED)};
+
 Gfx *getHumanDL(Gfx *dl) {
     return GlobalObjects_getGlobalGfxPtr(OBJECT_LINK_CHILD, dl);
+}
+
+static void makeTunicColorable(Gfx *dl) {
+    while (dl->words.w0 >> 24 != G_ENDDL) {
+        if (dl->words.w0 >> 24 == G_DL) {
+            makeTunicColorable((Gfx *)dl->words.w1);
+
+            if (((dl->words.w0 >> 16) & 0xFF) == G_DL_NOPUSH) {
+                break;
+            }
+        }
+
+        if (YAZMTCore_Utils_MemCmp(sPrimTunicCombinerDL, dl, sizeof(sPrimTunicCombinerDL)) == 0) {
+            *dl = sEnvTunicCombinerDL[0];
+        }
+
+        dl++;
+    }
 }
 
 void registerHuman(void) {
@@ -25,6 +51,14 @@ void registerHuman(void) {
     gHumanModelHandle = PlayerModelManager_registerModel(PMM_API_VERSION, "__mm_object_link_child__", PMM_MODEL_TYPE_CHILD);
 
     FallbackModelsCommon_doCommonAssignments(gHumanModelHandle, skel, human, GlobalObjects_getGlobalObject(GAMEPLAY_KEEP));
+
+    for (u8 i = 0; i < skel->sh.limbCount; ++i) {
+        LodLimb *curr = skel->sh.segment[i];
+
+        if (curr->dLists[0]) {
+            makeTunicColorable(curr->dLists[0]);
+        }
+    }
 
     PlayerModelManager_setDisplayList(gHumanModelHandle, LINK_DL_OPT_LFIST, getHumanDL(gLinkHumanLeftHandClosedDL));
     PlayerModelManager_setDisplayList(gHumanModelHandle, LINK_DL_OPT_LHAND_BOTTLE, getHumanDL(gLinkHumanLeftHandHoldBottleDL));
