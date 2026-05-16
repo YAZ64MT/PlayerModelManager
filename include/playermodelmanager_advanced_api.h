3221cbcbff9dd0949a0c01f216052720fd6b127c
Neirn <neirny@gmail.com>
2026-05-16 11:13:59 -0700

    Add missing PlayerModelManager_Actor_getFormModelType import

--- include/playermodelmanager_advanced_api.h
+++ include/playermodelmanager_advanced_api.h
@@ -49,6 +49,8 @@ RECOMP_IMPORT(YAZMT_PMM_MOD_NAME, bool PlayerModelManager_Actor_setFormModelType
 
 // TODO: DOCUMENT THIS HEADER
 
+RECOMP_IMPORT(YAZMT_PMM_MOD_NAME, PlayerModelManagerModelType PlayerModelManager_Actor_getFormModelType(Actor *actor));
+
 RECOMP_IMPORT(YAZMT_PMM_MOD_NAME, bool PlayerModelManager_Actor_getModelInternalName(Actor *actor, PlayerModelManagerModelType type, char outBuf[], size_t outBufSize));
 
 RECOMP_IMPORT(YAZMT_PMM_MOD_NAME, TexturePtr PlayerModelManager_Actor_getEyesTexturePtr(Actor *actor, PlayerEyeIndex index));
